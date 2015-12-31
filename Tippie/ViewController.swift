b35706d481bf0441ad038fb5ca7bf98d54066533
Limoncilla <Limoncilla@Limoncillas-Mac.local>
2015-12-31 14:27:00 -0800

    Here is my commit message

--- Tippie/ViewController.swift
+++ Tippie/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+ override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    tipLabel.text = "$0.00"
+    totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged
+        (sender: AnyObject) {
+        var tipPercentages = [0.15, 0.18, 0.20]
+        var tipPercentage = tipPercentages [tipControl.selectedSegmentIndex]
+            
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+            
+        tipLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+    @IBAction func onTap(sender: AnyObject)
+        {
+        view.endEditing (true)
+        }
 
 }
-
+git remote add origin https://github.com/limoncilla/Pre-Work.git
+git push -u origin master
